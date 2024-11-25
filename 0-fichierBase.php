e61b87efc0d26b5bbfb0f3eb672ac1ce8fd99465
VirgileBernard <bernardvirgile@gmail.com>
2024-11-25 13:27:21 +0100

    Correction pendu

--- 0-fichierBase.php
+++ 0-fichierBase.php
@@ -3,27 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pendu Fichier de Base</title>
+    <title>Pendu step1 - Affichage erreur ou lettre </title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
 <?php
+
 //constante d'environnement.
-const NBR_CHANCE = 10;
+const NBR_CHANCE = 6;
 
 //variables qui viennent du formulaire
-$lettreProposee = "a"; //la lettre proposée qui sera recue via un formulaire plus tard
+$lettreProposee = "v"; //la lettre proposée recue via un formulaire
 
-//variable externe, elle viendra d'un fichier reprenant une liste de mots à terme
-$motMystere = 'tablette'; //le mot à trouver
+//variable externe, elle viendra d'un fichier reprenant une liste de mots
+$motMystere = 'table'; //le mot à trouver
 
 //variables qui seront calculées à terme. Pour l'instant on indique "en dure" la valeur
-$nbrEchec = 0; // le nombre de faute
+$nbrEchec = 4; // le nombre de fautes
 $lettres = 't,b'; // les lettres trouvées séparées par une virgule.
-echo NBR_CHANCE;
+$ok = true; // la lettre a été trouvée ou pas
+
+//variables "calculées"
+$resteChance = 'Il te reste ' . (NBR_CHANCE - $nbrEchec) . ' chances';
+
+if ($ok == true) {
+    $message = "Bravo! La lettre existe dans le mot. " . $resteChance;
+} else $message = "Pas de chance... Cette lettre n'est pas de le mot " . $resteChance;
+
+if (NBR_CHANCE === $nbrEchec) $message = "Game Over";
 ?>
+
+<div><?php echo $message ?></div>
 <div class="pendu">
+
     <div class="potence">
         <div class="base"></div>
         <div class="pilier"></div>
@@ -31,17 +44,18 @@ echo NBR_CHANCE;
         <div class="corde"></div>
     </div>
     <div class="bonhomme">
-        <div class="tete"></div>
-        <div class="corps"></div>
-        <div class="bras bras-gauche"></div>
-        <div class="bras bras-droit"></div>
-        <div class="jambe jambe-gauche"></div>
-        <div class="jambe jambe-droit"></div>
+        <?php
+        if ($nbrEchec > 0) echo '<div class="tete"></div>';
+        if ($nbrEchec > 1) echo '<div class="corps"></div>';
+        if ($nbrEchec > 2) echo ' <div class="bras bras-gauche"></div>';
+        if ($nbrEchec > 3) echo ' <div class="bras bras-droit"></div>';
+        if ($nbrEchec > 4) echo ' <div class="jambe jambe-gauche"></div>';
+        if ($nbrEchec > 5) echo ' <div class="jambe jambe-droit"></div>';
+        ?>
     </div>
 </div>
 <div class="reponse">
-
-
+    Lettres trouvée: <?php echo $lettres ?>
 </div>
 </body>
 </html>
